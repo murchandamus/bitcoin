deb49eafde3e1ed303d8a84b9cb7f3112db7d124
Murch <murch@murch.one>
2022-06-24 12:36:56 -0400

    Move change size calculation to after output creation

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -749,17 +749,6 @@ static std::optional<CreatedTransactionResult> CreateTransactionInternal(
         return std::nullopt;
     }
 
-    // The output counter is encoded as a CompactSize. If adding a change output would increase the output counter's encoding length, increase change_output_size.
-    coin_selection_params.change_output_size += GetSizeOfCompactSize(txNew.vout.size() + 1) - GetSizeOfCompactSize(txNew.vout.size());
-
-    // Calculate the cost of change
-    // Cost of change is the cost of creating the change output + cost of spending the change output in the future.
-    // For creating the change output now, we use the effective feerate.
-    // For spending the change output in the future, we use the discard feerate for now.
-    // So cost of change = (change output size * effective feerate) + (size of spending change output * discard feerate)
-    coin_selection_params.m_change_fee = coin_selection_params.m_effective_feerate.GetFee(coin_selection_params.change_output_size);
-    coin_selection_params.m_cost_of_change = coin_selection_params.m_discard_feerate.GetFee(coin_selection_params.change_spend_size) + coin_selection_params.m_change_fee;
-
     // vouts to the payees
     coin_selection_params.tx_noinputs_size = 11; // Static vsize overhead + outputs vsize. 4 nVersion, 4 nLocktime, 1 input count, 1 output count, 1 witness overhead (dummy, flag) // TODO: overpays by 0.5 vB
     for (const auto& recipient : vecSend)
@@ -778,6 +767,17 @@ static std::optional<CreatedTransactionResult> CreateTransactionInternal(
         }
         txNew.vout.push_back(txout);
     }
+
+    // The output counter is encoded as a CompactSize. If adding a change output would increase the output counter's encoding length, increase change_output_size.
+    coin_selection_params.change_output_size += GetSizeOfCompactSize(txNew.vout.size() + 1) - GetSizeOfCompactSize(txNew.vout.size());
+
+    // Calculate the cost of change
+    // Cost of change is the cost of creating the change output + cost of spending the change output in the future.
+    // For creating the change output now, we use the effective feerate.
+    // For spending the change output in the future, we use the discard feerate for now.
+    // So cost of change = (change output size * effective feerate) + (size of spending change output * discard feerate)
+    coin_selection_params.m_change_fee = coin_selection_params.m_effective_feerate.GetFee(coin_selection_params.change_output_size);
+    coin_selection_params.m_cost_of_change = coin_selection_params.m_discard_feerate.GetFee(coin_selection_params.change_spend_size) + coin_selection_params.m_change_fee;
 
     // Include the fees for things that aren't inputs, excluding the change output
     const CAmount not_input_fees = coin_selection_params.m_effective_feerate.GetFee(coin_selection_params.tx_noinputs_size);
