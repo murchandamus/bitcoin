74bc3e1777662d401296be391235e0be377cea5d
Murch <murch@murch.one>
2022-12-15 13:22:28 -0500

    Update unprocessed_count to to_process_count

--- src/txmempool.h
+++ src/txmempool.h
@@ -701,7 +701,10 @@ public:
                                    std::string& errString,
                                    bool fSearchForParents = true) const EXCLUSIVE_LOCKS_REQUIRED(cs);
 
-    /** Get entire list of connected transactions for all transactions in txids. */
+    /**
+     * Get entire list of connected transactions for all transactions in txids.
+     * @pre All txs must be in the mempool, else an empty vector is returned
+     */
     std::vector<txiter> CalculateCluster(const std::vector<uint256>& txids) const EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     /** Calculate all in-mempool ancestors of a set of transactions not already in the mempool and
